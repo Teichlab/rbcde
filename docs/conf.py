5bee995141bd6ea87cbf2da390aba0e2cb93b549
ktpolanski <krzysztof_polanski@o2.pl>
2020-01-07 16:19:59 +0000

    mock import pandas

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
-
+autodoc_mock_imports = ['pandas']
 
 # -- Project information -----------------------------------------------------
 
